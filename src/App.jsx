4bd3b1c73183db8c74c745ca3f60844391923839
abelsanxd <abelsan4665@gmail.com>
2024-04-21 23:49:48 -0300

    se creo la base para el ejercicio 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main className="container my-5">
+        <h1>Hello world</h1>
+      </main>
+      ;
+    </>
+  );
+}
+
+export default App
